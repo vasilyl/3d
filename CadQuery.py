dade129fde6960ff3f58fd45f76a134e086d435e
vasilyl <3472644+vasilyl@users.noreply.github.com>
2025-11-07 22:43:51 -0800

    Move requirements.txt to the root directory

---
+++ CadQuery.py
@@ -0,0 +1,5 @@
+import cadquery as cq
+
+result = cq.Workplane("front").box(2.0, 2.0, 0.5)
+
+result
--- cq-editor.py
+++ cq-editor.py
@@ -1,4 +1,13 @@
 import cadquery as cq
 result = cq.Workplane("XY").box(10000, 7000, 5000)
 
-show_object(result)
+assembly = cq.Assembly()
+assembly.add(
+    result,
+    name="Result",
+    # color=cq.Color(0.2, 0.4, 0.6) # Optional: Adds a color
+)
+
+assembly.export(path="result.glb")
+
+result
